b001337e4cdb20feee7a0a865c13a6e193af7a15
Dominik Mlynek <dominik.mlynek@colossusbets.com>
2019-10-27 18:02:49 +0000

    Add Post action and basic validation

--- src/main/java/com/app/todo/controller/TodoController.java
+++ src/main/java/com/app/todo/controller/TodoController.java
@@ -3,9 +3,18 @@ package com.app.todo.controller;
 import com.app.todo.model.Todo;
 import com.app.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class TodoController {
@@ -13,7 +22,7 @@ public class TodoController {
     @Autowired
     TodoRepository er;
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
 
 
@@ -22,7 +31,7 @@ public class TodoController {
         todo1.setTitle("title1");
         todo1.setDescription("desc1");
 
-        er.save(todo1);
+        //er.save(todo1);
 
         Todo todo2 = new Todo();
         todo2.setTitle("title2");
@@ -32,6 +41,18 @@ public class TodoController {
 
         model.addAttribute("todos", todos);
         //model.addAttribute("todos", todos);
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public String create(Model model, @Valid @ModelAttribute Todo todo, BindingResult bindingResult) {
+
+        er.save(todo);
+
+        Iterable<Todo> todos = er.findAll();
+
+        model.addAttribute("todos", todos);
 
         return "index";
     }
