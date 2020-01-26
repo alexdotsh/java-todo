8fff15d9c9d8351e842016f41d1d8968abbd34ee
Dominik Mlynek <dominik.mlynek@colossusbets.com>
2020-01-26 13:43:58 +0000

    Logging functionality on persisting database

--- src/main/java/com/app/todo/controller/TodoController.java
+++ src/main/java/com/app/todo/controller/TodoController.java
@@ -6,6 +6,8 @@ import com.app.todo.repository.TodoRepository;
 import com.app.todo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Validator;
@@ -36,8 +38,9 @@ public class TodoController {
     public String index(Model model) {
 
         Iterable<Todo> todos = todo_repository.findAll();
-
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("todos", todos);
+        model.addAttribute("principal", auth.getPrincipal());
 
         return "todo/index";
     }
