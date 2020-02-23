1f39b263f6673e475718b97652f00f99aead75dd
Dominik Mlynek <dominik.mlynek@colossusbets.com>
2020-02-23 16:16:08 +0000

    Tests for OAuth

--- src/main/java/com/app/todo/repository/UserRepository.java
+++ src/main/java/com/app/todo/repository/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByTypeAndUsername(String type, String username);
+    Optional<User> findByTypeAndExternalId(String type, String externalId);
 
 }
--- src/main/java/com/app/todo/services/MyOAuth2UserService.java
+++ src/main/java/com/app/todo/services/MyOAuth2UserService.java
@@ -1,6 +1,8 @@
 package com.app.todo.services;
 
 import com.app.todo.model.FacebookPrincipal;
+import com.app.todo.model.FacebookUser;
+import com.app.todo.model.User;
 import com.app.todo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -9,6 +11,10 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class MyOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
@@ -23,13 +29,23 @@ public class MyOAuth2UserService implements OAuth2UserService<OAuth2UserRequest,
         myOAuth2UserService = new DefaultOAuth2UserService();
     }
 
+    public MyOAuth2UserService(OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService ){
+        System.out.println("MyOAuth2UserService");
+        myOAuth2UserService = oAuth2UserService;
+    }
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2Useruser = myOAuth2UserService.loadUser(userRequest);
-        FacebookPrincipal facebookPrincipal = new FacebookPrincipal(oAuth2Useruser);
+        Map userAttributes = oAuth2Useruser.getAttributes();
+        Optional<User> user = userRepository.findByTypeAndExternalId("Facebook", (String)userAttributes.get("id"));
+        if(user.isPresent()){
+            return (FacebookUser)user.get();
 
-
-
-        return facebookPrincipal;
+        } else {
+            FacebookUser newUser = new FacebookUser(oAuth2Useruser);
+            userRepository.save(newUser);
+            return newUser;
+        }
     }
 }
