fb73af4b6b6f45e2ccbc6e961636cd87477c84c7
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-03-14 22:28:49 +0000

    Refactored todo controller

--- src/main/java/com/app/todo/controller/TodoController.java
+++ src/main/java/com/app/todo/controller/TodoController.java
@@ -4,12 +4,8 @@ import com.app.todo.model.Todo;
 import com.app.todo.repository.TodoRepository;
 import com.app.todo.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Validator;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +15,6 @@ import javax.validation.Valid;
 public class TodoController {
     @Autowired
     private TodoRepository todoRepository;
-    @Autowired
-    Validator validator;
-    @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService;
     @Autowired
     private TodoService todoService;
 
@@ -35,22 +27,27 @@ public class TodoController {
         return "todo/index";
     }
 
-    @GetMapping("todos/new")
-    public String newTodo(Todo todo) { return "todo/new"; }
+    @GetMapping("todos/create")
+    public String create(Model model) {
+        model.addAttribute("todo", new Todo());
+
+        return "todo/new";
+    }
 
     @PostMapping("todos/create")
     public String create(@Valid @ModelAttribute Todo todo, BindingResult bindingResult) {
-        if (!bindingResult.hasErrors()){
-            todoService.save(todo);
 
-            return "redirect:/";
-        } else {
+        if (bindingResult.hasErrors()) {
             return "todo/new";
         }
+
+        todoService.save(todo);
+
+        return "redirect:/";
     }
 
-    @RequestMapping(value = "todos/{Id}/update", method = RequestMethod.GET)
-    public String updateTodo(@PathVariable Long Id, Model model) {
+    @GetMapping("todos/{Id}/update")
+    public String update(@PathVariable Long Id, Model model) {
         Todo todo = todoRepository.findById(Id).get();
         model.addAttribute("todo", todo);
 
@@ -58,7 +55,7 @@ public class TodoController {
     }
 
     @GetMapping("todos/{Id}/delete")
-    public String deleteTodo(@PathVariable Long Id) {
+    public String delete(@PathVariable Long Id) {
         todoService.delete(Id);
 
         return "redirect:/";
