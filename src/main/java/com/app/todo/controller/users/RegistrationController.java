88d71d9b798c5d18fed0fb348391d58238a840e9
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-03-14 22:29:23 +0000

    Removed unused package

--- src/main/java/com/app/todo/controller/users/RegistrationController.java
+++ src/main/java/com/app/todo/controller/users/RegistrationController.java
@@ -3,7 +3,6 @@ package com.app.todo.controller.users;
 import com.app.todo.model.User;
 import com.app.todo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
