60eec2d89afb4b9b8e69fc0019b9ec05c22d915b
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-03-18 23:47:40 +0000

    WIP - ability to access user account

---
+++ src/main/java/com/app/todo/controller/users/AccountController.java
@@ -0,0 +1,38 @@
+package com.app.todo.controller;
+
+import com.app.todo.model.User;
+import com.app.todo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Controller
+public class AccountController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("user/account")
+    public String index(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = ((AuthenticatedPrincipal)principal).getName();
+        }
+
+        User user = userRepository.findByUsername(username);
+
+        model.addAttribute("user", user);
+
+        return "user/index";
+    }
+
+    @GetMapping("user/{Id}/update")
+    public String update(@PathVariable Long Id, Model model) {}
+}
