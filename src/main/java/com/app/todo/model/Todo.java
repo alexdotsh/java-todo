e9efa678bdb61034d3e8a26333e3b5648426560a
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-04-04 18:23:05 +0100

    Done field with default value of false

--- src/main/java/com/app/todo/model/Todo.java
+++ src/main/java/com/app/todo/model/Todo.java
@@ -16,23 +16,25 @@ public class Todo {
     @Size(min = 5, message = "Description need to be longer")
     private String Description;
 
+    @Column(columnDefinition = "boolean default false")
+    private Boolean Done = false;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Long getId() { return id; }
-
     public void setId(Long id) { this.id = id; }
 
     public String getTitle() { return Title; }
-
     public void setTitle(String Title) { this.Title = Title; }
 
     public String getDescription() { return Description; }
-
     public void setDescription(String Description) { this.Description = Description; }
 
     public User getUser() { return user; }
-
     public void setUser(User user) { this.user = user; }
+
+    public Boolean getDone() { return Done; }
+    public void setDone(Boolean Done) { this.Done = Done; }
 }
